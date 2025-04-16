c56e929f8d33bf3685102c9de89c4b539e921c3b
Aleksey Pechin <pechinalex412112@yandex>
2025-04-16 09:47:34 +0300

    adjusting the ArrayList name

--- src/model/Epic.java
+++ src/model/Epic.java
@@ -4,29 +4,29 @@ import java.util.ArrayList;
 
 public class Epic extends Task {
 
-    private ArrayList<Integer> idSubtaskArrays = new ArrayList<>();
+    private ArrayList<Integer> idSubtasks = new ArrayList<>();
 
     public Epic(String name, String description, Status status, Type type) {
         super(name, description, status, type);
     }
 
-    public ArrayList<Integer> getIdSubtaskArrays() {
-        return idSubtaskArrays;
+    public ArrayList<Integer> getIdSubtasks() {
+        return idSubtasks;
     }
 
-    public void setIdSubtaskArrays(ArrayList<Integer> idSubtaskArrays) {
-        this.idSubtaskArrays = idSubtaskArrays;
+    public void setIdSubtasks(ArrayList<Integer> idSubtasks) {
+        this.idSubtasks = idSubtasks;
     }
 
     public void clearIdSubtaskArrays() {
-        idSubtaskArrays.clear();
+        idSubtasks.clear();
     }
 
     public void addIdSubtask(int idSubtask) {
-        idSubtaskArrays.add(idSubtask);
+        idSubtasks.add(idSubtask);
     }
 
     public void removeSubtaskById(int idSubtask) {
-        idSubtaskArrays.remove(idSubtask);
+        idSubtasks.remove(idSubtask);
     }
 }

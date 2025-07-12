bd559ed022be308257b5afc036d8e83344b747fa
Aleksey Pechin <pechinalex412112@yandex>
2025-07-12 12:28:27 +0300

    HttpTaskServer class created

---
+++ src/main/http/HttpTaskServer.java
@@ -0,0 +1,39 @@
+package main.http;
+
+import com.sun.net.httpserver.HttpServer;
+import main.http.handlers.*;
+import main.managers.Managers;
+import main.taskManagerAndHistoryManagerInterfaces.TaskManager;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+public class HttpTaskServer {
+
+    private final HttpServer httpServer;
+    private static final int PORT = 8080;
+
+    public HttpTaskServer(TaskManager taskManager) throws IOException {
+        this.httpServer = HttpServer.create();
+        httpServer.bind(new InetSocketAddress(PORT), 0);
+        httpServer.createContext("/task", new TaskHandler(taskManager));
+        httpServer.createContext("/epic", new EpicHandler(taskManager));
+        httpServer.createContext("/subtask", new SubtaskHandler(taskManager));
+        httpServer.createContext("/history", new HistoryHandler(taskManager));
+        httpServer.createContext("/prioritized", new PrioritizedHandler(taskManager));
+    }
+
+    public static void main(String[] args) throws IOException {
+        TaskManager taskManager = Managers.getInMemoryTaskManager(Managers.getDefaultHistory());
+        HttpTaskServer httpTaskServer = new HttpTaskServer(taskManager);
+        httpTaskServer.start();
+    }
+
+    public void start() {
+        httpServer.start();
+    }
+
+    public void stop() {
+        httpServer.stop(0);
+    }
+}
